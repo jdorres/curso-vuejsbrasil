e6547427038ed3ed46b811e3d2cf210bf061af83
Julio <juliodorres@gmail.com>
2024-08-06 22:38:56 -0300

    criada página da home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,42 @@
 <template>
-  <div><h1 >Aqui é a Home 2</h1></div>
+  <custom-header 
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <contact-component />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker 2021</p>
+  </div>
 </template>
 
 <script>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import contactComponent from './contactComponent.vue'
+import customHeader from './customHeader.vue'
+
 export default {
-    name: 'homePage'
+  name: 'homePage',
+  components: { contactComponent, customHeader },
+  setup () {
+    const router = useRouter()
+    onMounted(() => {
+      const token =window.localStorage.getItem('token')
+      if(token){
+        router.push({name: 'feedbacks'})
+      }
+    })
+
+    function handleLogin (){
+    }
+    function handleAccountCreate(){
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate,
+    }
+  }
 }
 </script>
 
